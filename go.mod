8edb0128a88fd09eb48cd3fbd85f5b15516a324c
tijnstolwijk <tijn.stolwijk@gmail.com>
2024-07-11 16:52:48 +0200

    't werkt gewoon

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module teletekst
 
 go 1.22.4
+
+require github.com/h2so5/goback v0.0.0-20150302055225-6e210305bfc9
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "os"
-  "fmt"
-  "slices"
-  "strings"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/h2so5/goback/regexp"
 )
 
 type teletekst_pagina struct{
@@ -25,7 +28,26 @@ type fastTextLink struct {
 }
 
 func main(){
-  url := "https://teletekst-data.nos.nl/json/101" 
+  printPage(101)
+  
+  var input string
+  for {
+    fmt.Print("Pagina: ")
+    fmt.Scan(&input)
+    pageNum, err := strconv.ParseInt(input, 10, 0)
+    if err == nil {
+      fmt.Println("\033[2J")
+      printPage(int(pageNum))
+    }else{
+      if input == "q"{
+        os.Exit(0)
+      }
+    }
+  }
+}
+
+func printPage(page int) {
+  url := fmt.Sprintf("https://teletekst-data.nos.nl/json/%d", page)
   response, err := http.Get(url)
   if err != nil {
     fmt.Printf("Error getting to teletekst\n")
@@ -34,35 +56,56 @@ func main(){
   var pagina teletekst_pagina 
   err = json.NewDecoder(response.Body).Decode(&pagina)
   lines := strings.Split(pagina.Content, "\n")
-  //for i := 0; i < len(lines); i++ {
-  //  processed_line := processHTML(lines[i])
-  //  fmt.Printf(processed_line)
-  //}
-  fmt.Printf(lines[0] + "\n")
-  fmt.Printf(processHTML(lines[0]) + "\n")
-
+  for i := 0; i < len(lines); i++ {
+    processed_line := processHTML(lines[i])
+    processed_line = replaceBlockCharsR(processed_line)
+    fmt.Printf("%v\n", processed_line)
+  }
 }
 
+
+// Text processing functions
 func processHTML(line string) string{
   // Remove all HTML tags
   chars := strings.Split(line, "")
+
   var begin int
   var end int
+
   for {
-    if !slices.Contains(chars, "<"){
+    if !slices.Contains(chars, "<") && !slices.Contains(chars, ">"){
       break
     }
+
     for i := 0; i < len(chars); i++{
       if chars[i] == "<"{
         begin = i
+        //fmt.Printf("%v found at i=%d\n", chars[i], i)
       }
       if chars[i] == ">"{
         end = i
+        //fmt.Printf("%v found at i=%d\n", chars[i], i)
+        //fmt.Printf("Deleting from %v to %v\n", chars[begin], chars[end])
         chars = slices.Delete(chars, begin, end + 1)
+        //fmt.Printf(strings.Join(chars, "")+"\n")
+        break
       }
     }
 
   }
   return strings.Join(chars, "")
+}
+
+func replaceBlockCharsR(line string) string {
+  blockChars := regexp.MustCompile("&#xF0(?!20;).{3}")
+  result := blockChars.ReplaceAll([]byte(line), []byte("%"))
+  blackBlockChars := regexp.MustCompile("&#xF020;")
+  result = blackBlockChars.ReplaceAll(result, []byte(" "))
+  return string(result)
+}
+
+func replaceSpecialChars(line string) string{
+  // https://www.html.am/reference/html-special-characters.cfm ISO 8859-1 section
+  return ""
 }
 
